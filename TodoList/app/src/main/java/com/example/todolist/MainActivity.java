44562ec71c8f5c0aa2131a2815c5b43a6e7f8ab7
sabsabila <58177825+sabsabila@users.noreply.github.com>
2020-09-17 12:45:24 +0700

    Fix disappeared action bar

--- TodoList/app/src/main/java/com/example/todolist/MainActivity.java
+++ TodoList/app/src/main/java/com/example/todolist/MainActivity.java
@@ -8,16 +8,18 @@ import android.app.Activity;
 import android.view.Menu;
 import android.widget.ArrayAdapter;import android.widget.ListView;
 
-public class MainActivity extends Activity
+public class MainActivity extends AppCompatActivity
 {
-    // Array of strings...
-    ListView simpleList;
-    String countryList[] = {"Membuat rangkuman Layout", "Mengerjakan Tugas Machine Learning", "Mengerjakan Tugas Keterampilan Komunikasi", "Bermain Game"};
+    String[] mobileArray = {"Membuat rangkuman Layout", "Mengerjakan tugas Machine Learning", "Mengerjakan tugas Keterampilan Komunikasi", "Bermain game"};
 
-    @Override   protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);      setContentView(R.layout.activity_main);
-        simpleList = (ListView)findViewById(R.id.simpleListView);
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.activity_listview, R.id.textView, countryList);
-        simpleList.setAdapter(arrayAdapter);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.activity_listview, mobileArray);
+
+        ListView listView = (ListView) findViewById(R.id.mobile_list);
+        listView.setAdapter(adapter);
     }
 }
